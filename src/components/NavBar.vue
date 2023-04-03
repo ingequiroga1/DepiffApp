90068fcc86d07a60a23f3a62e95ab3ac671f752b
ingequiroga1 <ingequiroga1@gmail.com>
2023-04-03 14:36:01 -0500

    Se agregan archivos y gitignore

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary fixed-top">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" aria-current="page" href="#" @click="moveToSection('hero')">Home</a>
+        <a class="nav-link" href="#" @click="moveToSection('nosotros')">Features</a>
+        <a class="nav-link" href="#" @click="moveToSection('acerca')">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default{
+    methods: {
+        moveToSection(id){
+            //console.log(id);
+            this.$emit('goToSeccion',id)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+
+.navbar-nav a{
+    color: #fff;
+    font-weight: bold;
+    height: 70px;
+    line-height: 70px;
+    padding: 0 15px;
+    font-size: 1em;
+    text-transform: uppercase;
+    font-family: 'Trebuchet MS', sans-serif;
+    -webkit-transition: height .4s ease, line-height .4s ease, margin .4s ease, padding .4s ease;
+    -o-transition: height .4s ease, line-height .4s ease, margin .4s ease, padding .4s ease;
+    transition: height .4s ease, line-height .4s ease, margin .4s ease, padding .4s ease;
+}
+
+.navbar-nav a:hover  {
+  background: #ffffff;
+  border-color:#ffffff;
+  color: #F2921D;
+}
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<template>
+  <NavBar @goToSeccion = "moveToSection($event)"/>
+  <div id="hero" class="col-md-12 fazul text-center">
+      <img src="../assets/images/LOGODEPIFF.png">
+      <h1 style="font-size: 5em; padding-top: 15%; padding-bottom:15%">Depiff</h1>
+  </div>
+  <div id="nosotros"></div>
+  <div id="acerca"></div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'HomeView',
+  methods:{
+    moveToSection(event){
+     // console.log(event);
+       let element = document.getElementById(event)
+       let x = element.offsetLeft;
+       let y = element.offsetTop;
+
+       window.scrollTo(x,y);
+    }
+  },
+  components: {
+   // HelloWorld,
+    NavBar
+  }
+}
+</script>
+
+<style>
+#hero
+{
+  background: red;
+  width: 100%;
+  height: 100vh;
+}
+
+#nosotros
+{
+  background: yellow;
+  width: 100%;
+  height: 100vh;
+}
+#acerca
+{
+  background: blue;
+  width: 100%;
+  height: 100vh;
+}
+
+</style>
